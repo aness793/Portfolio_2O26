1ab0de37b60d4f2c598c5e04e19a33e0810348cd
aness793 <aness12317@gmail.com>
2026-07-22 20:00:02 +0100

    fix cvs download window

--- src/landing.jsx
+++ src/landing.jsx
@@ -23,7 +23,7 @@ export default function Landing() {
               </div>
               <div className="cvs-content">
                   <span>
-                    <img draggable='false' src="download.png" alt="" />
+                    {/* <img draggable='false' src="download.png" alt="" /> */}
                     <p>Machine Learning</p>
                     <a href="Resume_Aness_Rahmani_ML_Engineer.pdf" download='Aness_Rahmani_ML' >
                                     <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 448 512"><path d="M256 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 210.7-41.4-41.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 242.7 256 32zM64 320c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-46.9 0-56.6 56.6c-31.2 31.2-81.9 31.2-113.1 0L110.9 320 64 320zm304 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
@@ -31,7 +31,7 @@ export default function Landing() {
                     </a>
                 </span>
                   <span>
-                    <img draggable='false' src="download.png" alt="" />
+                    {/* <img draggable='false' src="download.png" alt="" /> */}
                     <p>Full-stack Development </p>
                     <a href="Resume_Aness_Rahmani_FullStack_Developer.pdf" download='Aness_Rahmani_Fullstack' >
                                     <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 448 512"><path d="M256 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 210.7-41.4-41.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 242.7 256 32zM64 320c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-46.9 0-56.6 56.6c-31.2 31.2-81.9 31.2-113.1 0L110.9 320 64 320zm304 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
@@ -39,7 +39,7 @@ export default function Landing() {
                     </a>
                 </span>
                   <span>
-                    <img draggable='false' src="download.png" alt="" />
+                    {/* <img draggable='false' src="download.png" alt="" /> */}
                     <p>Backend Development</p>
                     <a href="Resume_Aness_Rahmani_Backend_Developer.pdf" download='Aness_Rahmani_Backend' >
                                     <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 448 512"><path d="M256 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 210.7-41.4-41.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 242.7 256 32zM64 320c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-46.9 0-56.6 56.6c-31.2 31.2-81.9 31.2-113.1 0L110.9 320 64 320zm304 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
@@ -47,14 +47,16 @@ export default function Landing() {
                     </a>
                 </span>
                 <span>
-                    <div><img draggable='false' src="download.png" alt="" /></div>
+                    {/* <div>
+                      <img draggable='false' src="download.png" alt="" />
+                    </div> */}
                     <p>Frontend Development</p>
                     <a href="Resume_Aness_Rahmani_Frontend_Developer.pdf" download='Aness_Rahmani_Frontend' >
                       <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 448 512"><path d="M256 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 210.7-41.4-41.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 242.7 256 32zM64 320c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-46.9 0-56.6 56.6c-31.2 31.2-81.9 31.2-113.1 0L110.9 320 64 320zm304 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
                     </a>
                 </span>
                 <span>
-                    <img draggable='false' src="download.png" alt="" />
+                    {/* <img draggable='false' src="download.png" alt="" /> */}
                     <p>Data Specialist</p>
                     <a href="Resume_Aness_Rahmani_AI_Data_Specialist.pdf" download='Aness_Rahmani_Data_specialist' >
                       <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 448 512"><path d="M256 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 210.7-41.4-41.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l96 96c12.5 12.5 32.8 12.5 45.3 0l96-96c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 242.7 256 32zM64 320c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-46.9 0-56.6 56.6c-31.2 31.2-81.9 31.2-113.1 0L110.9 320 64 320zm304 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
